c5a342a9f712cb7f27b593d8d1c392f2d4643cd7
Hadywahba <hadywahba19@gmail.com>
2025-09-27 13:04:33 +0300

    fix: show default text when description is missing

--- src/components/features/TaskCard/TaskCard.tsx
+++ src/components/features/TaskCard/TaskCard.tsx
@@ -66,7 +66,7 @@ export default function TaskCard({
         </h5>
 
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {description}
+          {description ? description.split(' ').splice(0, 4).join(' ') : "No description available"}
         </p>
       </div>
       <span
